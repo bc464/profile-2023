c4993f5a5240dd7e0400dd7d14ed9c4ecf56f481
bc464 <fil419@webmail.co.za>
2023-03-05 13:13:47 +0200

    added frontend project image- skilled e-learning

--- script.js
+++ script.js
@@ -52,12 +52,7 @@ window.onclick = function(event) {
 		modal.style.display = "none";
 	}
 }
-// NAVBAR CLICK DROPDOWN
-// function myFunction() {
-//   document.getElementById("myDropdown").classList.toggle("show");
-// }
 
-// Close the dropdown if the user clicks outside of it
 window.onclick = function(e) {
   if (!e.target.matches('.dropbtn')) {
   let myDropdown = document.getElementById("myDropdown");
