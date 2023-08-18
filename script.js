13955c9e735ee88d5499c09a0ffc0789410571ec
bc464 <fil419@webmail.co.za>
2023-08-18 16:18:11 +0200

    some further updated and changes to project

--- script.js
+++ script.js
@@ -1,40 +1,3 @@
-const slides = document.querySelectorAll(".slide");
-const nextBtn = document.querySelector(".nextBtn");
-const prevBtn = document.querySelector(".prevBtn");
-
-slides.forEach(function (slide, index) {
-	slide.style.left = `${index * 100}%`;
-});
-
-let counter = 0;
-nextBtn.addEventListener("click", function() {
-	counter++;
-	carousel();
-});
-prevBtn.addEventListener("click", function() {
-	counter--;
-	carousel();
-});
-
-function carousel() {
-	if (counter < slides.length -1) {
-		nextBtn.style.display = "block";
-	} else {
-		nextBtn.style.display = "none";
-	}
-	if (counter > 0) {
-		prevBtn.style.display = "block";
-	} else {
-		prevBtn.style.display = "none";
-	}
-	slides.forEach(function (slide) {
-		slide.style.transform = `translateX(-${counter * 100}%`;
-	});
-}
-
-prevBtn.style.display = "none";
-
-
 // MODAL
 let modal = document.getElementById("myModal");
 let btn = document.getElementById('myBtn');
@@ -53,12 +16,17 @@ window.onclick = function(event) {
 	}
 }
 
-window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn')) {
-  let myDropdown = document.getElementById("myDropdown");
-    if (myDropdown.classList.contains('show')) {
-      myDropdown.classList.remove('show');
-    }
-  }
-}
+
+
+
+
+
+// window.onclick = function(e) {
+//   if (!e.target.matches('.dropbtn')) {
+//   let myDropdown = document.getElementById("myDropdown");
+//     if (myDropdown.classList.contains('show')) {
+//       myDropdown.classList.remove('show');
+//     }
+//   }
+// }
 
